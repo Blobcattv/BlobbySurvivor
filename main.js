4a6e1077400baf05bf66f104e2c64f6eb1d588ff
Mina <blobcattv@gmail.com>
2023-07-19 20:28:54 +0200

    setup project

---
+++ main.js
@@ -0,0 +1,16 @@
+const config = {
+    width: 600,
+    height: 500,
+    backgroundColor: 0x20395,
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
+        }
+    },
+    zoom: 2,
+    scene: Scene
+}
+
+const game = new Phaser.Game(config);
