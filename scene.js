12ab35301812dc3d491bd8624ef4240f9dae50ec
Mina <blobcattv@gmail.com>
2023-07-22 15:08:48 +0200

    create enemy and follow logic

--- scene.js
+++ scene.js
@@ -11,6 +11,12 @@ class Scene extends Phaser.Scene {
             frameWidth: 33,
             frameHeight: 33
         });
+
+        // load enemy
+        this.load.spritesheet("enemy", "assets/Sprout Lands/Characters/Basic Charakter Spritesheet.png",{
+            frameWidth: 33,
+            frameHeight: 33
+        });
     }
 
     create(){
@@ -24,6 +30,21 @@ class Scene extends Phaser.Scene {
         this.cursorKeys = this.input.keyboard.createCursorKeys();
         this.player.setCollideWorldBounds(true);
 
+        // add "enemy"
+        this.enemies = this.physics.add.sprite(config.width, config.height, "enemy");
+
+        
+        // health bar
+        this.healthBar = this.makeBar(0,0,0xe7153c);
+        this.healtbarValue = 100;
+        this.setValue(this.healthBar, this.healtbarValue);
+        
+        // add physics to player and enemy
+        this.physics.add.collider(this.player, this.enemies, (player) => this.hurtPlayer(player, this.healthBar), null, this);
+        
+        // create container for player and healthbar
+        // const container = this.add.container(config.width / 2, config.height / 2, [ this.healthBar, this.player ]);
+        
         // camera logic to follow player
         this.cameras.main.setBounds(0, 0, 600 * 2, 500 * 2);
         this.physics.world.setBounds(0, 0, 600 * 2, 500 * 2);
@@ -32,9 +53,42 @@ class Scene extends Phaser.Scene {
 
     update(){
         this.movePlayerManager();
+        this.enemyFollows();
     }
 
-    // functions
+    hurtPlayer(player, bar) {
+        this.healtbarValue += 15;
+        const damage = 15;
+        bar.scaleX = (bar.scaleX - damage) /100;
+        // player.this.setTintFill(0xffffff);
+
+        // if (this.isTinted) {
+        //     this.clearTint();
+        // } else {
+        //     this.setTintFill(0xffffff);
+        // }
+    }
+
+    // takeDamage(player) {
+
+    // }
+
+    makeBar(x,y,color) {
+        const bar = this.add.graphics();
+
+        bar.fillStyle(color);
+        bar.fillRect(0, 0, 30, 5);
+
+        bar.x = x;
+        bar.y = y;
+        
+        return bar;
+    }
+
+    setValue(bar,percentage) {
+        bar.scaleX = percentage/100;
+    }
+
     movePlayerManager() {
         const Move = 200;
         this.player.setDrag(2000);
@@ -52,4 +106,7 @@ class Scene extends Phaser.Scene {
         }
     }
 
+    enemyFollows () {
+        this.physics.moveToObject(this.enemies, this.player, 100);
+    }
 }
